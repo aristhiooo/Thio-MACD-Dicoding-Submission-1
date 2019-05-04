e9b75497d8750d0b23e74363c7318c4eff98c771
ARISTIYO RAHADIAN AKBAR <44568681+aristhiooo@users.noreply.github.com>
2019-05-04 23:31:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,31 +2,173 @@
  <head>
  <Title>Formulir Calon Anggota UKM Seni UNM</Title>
  <style type="text/css">
- 	body { background-color: #FF4500; border-top: solid 10px #000;
- 	    color: #333; font-size: .85em; margin: 20; padding: 20;
- 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;}
+	 @import url(https://fonts.googleapis.com/css?family=Roboto:400,300,600,400italic);
+	 * {
+		 margin: 0;
+		 padding: 0;
+		 box-sizing: border-box;
+		 -webkit-box-sizing: border-box;
+		 -moz-box-sizing: border-box;
+		 -webkit-font-smoothing: antialiased;
+		 -moz-font-smoothing: antialiased;
+		 -o-font-smoothing: antialiased;
+		 font-smoothing: antialiased;
+		 text-rendering: optimizeLegibility;
+	 }
 	 
- 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
- 	h1 { font-size: 2em; }
- 	h2 { font-size: 1.75em; }
- 	h3 { font-size: 1.2em; }
- 	table { margin-top: 0.75em; }
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
- 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+	 body {
+		 font-family: "Roboto", Helvetica, Arial, sans-serif;
+		 font-weight: 100;
+		 font-size: 12px;
+		 line-height: 30px;
+		 color: #777;
+		 background: #4CAF50;
+	 }
+	 
+	 container {
+		 max-width: 400px;
+		 width: 100%;
+		 margin: 0 auto;
+		 position: relative;
+	 }
+	 
+	 #contact input[type="text"],
+	 #contact input[type="email"],
+	 #contact input[type="tel"],
+	 #contact input[type="url"],
+	 #contact textarea,
+	 #contact button[type="submit"] {
+		 font: 400 12px/16px "Roboto", Helvetica, Arial, sans-serif;
+	 }
+	 
+	 #contact {
+		 background: #F9F9F9;
+		 padding: 25px;
+		 margin: 150px 0;
+		 box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+	 }
+	 
+	 #contact h3 {
+		 display: block;
+		 font-size: 30px;
+		 font-weight: 300;
+		 margin-bottom: 10px;
+	 }
+	 #contact h4 {
+		 margin: 5px 0 15px;
+		 display: block;
+		 font-size: 13px;
+		 font-weight: 400;
+	 }
+	 
+	 fieldset {
+		 border: medium none !important;
+		 margin: 0 0 10px;
+		 min-width: 100%;
+		 padding: 0;
+		 width: 100%;
+	 }
+	 
+	 #contact input[type="text"],
+	 #contact input[type="email"],
+	 #contact input[type="tel"],
+	 #contact input[type="url"],
+	 #contact textarea {
+		 width: 100%;
+		 border: 1px solid #ccc;
+  background: #FFF;
+  margin: 0 0 5px;
+  padding: 10px;
+}
+
+#contact input[type="text"]:hover,
+#contact input[type="email"]:hover,
+#contact input[type="tel"]:hover,
+#contact input[type="url"]:hover,
+#contact textarea:hover {
+  -webkit-transition: border-color 0.3s ease-in-out;
+  -moz-transition: border-color 0.3s ease-in-out;
+  transition: border-color 0.3s ease-in-out;
+  border: 1px solid #aaa;
+}
+
+#contact textarea {
+  height: 100px;
+  max-width: 100%;
+  resize: none;
+}
+
+#contact button[type="submit"] {
+  cursor: pointer;
+  width: 100%;
+  border: none;
+  background: #4CAF50;
+  color: #FFF;
+  margin: 0 0 5px;
+  padding: 10px;
+  font-size: 15px;
+}
+
+#contact button[type="submit"]:hover {
+  background: #43A047;
+  -webkit-transition: background 0.3s ease-in-out;
+  -moz-transition: background 0.3s ease-in-out;
+  transition: background-color 0.3s ease-in-out;
+}
+
+#contact button[type="submit"]:active {
+  box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.5);
+}
+
+.copyright {
+  text-align: center;
+}
+
+#contact input:focus,
+#contact textarea:focus {
+  outline: 0;
+  border: 1px solid #aaa;
+}
+
+::-webkit-input-placeholder {
+  color: #888;
+}
+
+:-moz-placeholder {
+  color: #888;
+}
+
+::-moz-placeholder {
+  color: #888;
+}
+
+:-ms-input-placeholder {
+  color: #888;
+}
  </style>
  </head>
 	
  <body>
- <h1>Formulir Calon Anggota UKM Seni UNM</h1>
- <p>Silahkan isi formulir di bawah ini, kemudian klik <strong>Submit</strong> untuk daftar.</p>
- <form method="post" action="index.php" enctype="multipart/form-data" >
-       Nama Lengkap  <input type="text" name="nama" id="nama"/></br></br>
-       NIM <input type="text" name="nim" id="nim"/></br></br>
-       Program Studi <input type="text" name="prodi" id="prodi"/></br></br>
-       No. HP <input type="text" name="hp" id="hp"/></br></br>
-       <input type="submit" name="submit" value="DAFTAR" />
-       <input type="submit" name="load_data" value="LIHAT YANG TELAH MENDAFTAR" />
- </form>
+<form action="index.php" method="post" enctype="multipart/form-data">
+    <h3>Formulir Calon Anggota UKM Seni UNM</h3>
+    <h4><p>Silahkan isi formulir di bawah ini, kemudian klik <strong>Submit</strong> untuk daftar.</p></h4>
+    <fieldset>
+      <input name="nama" placeholder="Nama Lengkap" type="text" tabindex="1" id="nama" required autofocus>
+    </fieldset>
+    <fieldset>
+      <input name="nim" placeholder="NIM" type="text" tabindex="2" id="nim" required>
+    </fieldset>
+    <fieldset>
+      <input name="prodi" placeholder="Program Studi" type="text" tabindex="3" id="prodi" required>
+    </fieldset>
+    <fieldset>
+      <input name="hp" placeholder="Nomor Handphone" type="text" tabindex="4" required>
+    </fieldset>
+    <fieldset>
+      <input type="submit" name="submit" value="DAFTAR" />
+      <input type="submit" name="load_data" value="LIHAT YANG TELAH MENDAFTAR" />
+    </fieldset>
+  </form>
 
  <?php
     $host = "thiosqldatabase.database.windows.net";
